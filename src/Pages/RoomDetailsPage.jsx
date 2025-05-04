a06c58f28efc4f4a0b62144fd4f7ae2433bba074
Alejandro <agaviria1408@gmail.com>
2025-05-03 20:12:07 -0500

    Se configura la pagina RoomDetailsPage.jsx

--- src/Pages/RoomDetailsPage.jsx
+++ src/Pages/RoomDetailsPage.jsx
@@ -1,10 +1,13 @@
+import { useParams } from "react-router-dom";
 
 
 export default function RoomDetailsPage() {
+    //console.log("parametro",useParams());
+    let {id} =useParams();
   return (
     <div>
       <h1>Detalle de Habitación</h1>
-      <h2>Habitación #</h2>
+      <h2>Habitación #{id}</h2>
     </div>
   )
 }
