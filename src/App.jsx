99cbc4c31c62ad93b0f41b78b329585eca25c3b7
Alejandro <agaviria1408@gmail.com>
2025-05-03 19:11:51 -0500

    se configura rutas para las paginas

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,9 @@
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./Pages/HomePage";
+import AboutPage from "./Pages/AboutPage";
+import ContactPage from "./Pages/ContactPage";
+import NotFoundPage from "./Pages/NotFoundPage";
+import RoomsPage from "./Pages/RoomsPage";
 
 function App() {
 
